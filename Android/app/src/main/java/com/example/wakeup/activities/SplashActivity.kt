eef275d3efed58fbca048960127bfedd6173d698
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-01 23:07:01 +0900

    Android

---
+++ Android/app/src/main/java/com/example/wakeup/activities/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.wakeup.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.wakeup.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startLoading()
+    }
+
+    // 앱 실행 후 2초 뒤에 MainActivity로 전환
+    private fun startLoading() {
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
