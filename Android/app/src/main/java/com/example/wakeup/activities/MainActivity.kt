a9458ae05fb6a5a702767dbf1f4f787c3a6a72e2
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-03 01:51:21 +0900

    Bottom Navigation

--- Android/app/src/main/java/com/example/wakeup/activities/MainActivity.kt
+++ Android/app/src/main/java/com/example/wakeup/activities/MainActivity.kt
@@ -3,26 +3,65 @@ package com.example.wakeup.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.wakeup.R
+import com.example.wakeup.navigation.*
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
         when (p0.itemId) {
-            R.id.action_main -> {
-                val transaction =
-                    supportFragmentManager.beginTransaction() transaction . replace (R.id.frame, MainFragment()) transaction.commit() return true
+            R.id.home -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.frame, HomeFragment())
+                transaction.commit()
+                return true
             }
-                ... R.id.action_account
-            -> {
-                val transaction =
-                    supportFragmentManager.beginTransaction() transaction . replace (R.id.frame, AccountFragment()) transaction.commit() return true
+            R.id.focus -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.frame, FocusFragment())
+                transaction.commit()
+                return true
             }
-        } return false
+            R.id.device -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.frame, DeviceFragment())
+                transaction.commit()
+                return true
+            }
+            R.id.nosleep -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.frame, NoSleepFragment())
+                transaction.commit()
+                return true
+            }
+            R.id.time -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.frame, TimeFragment())
+                transaction.commit()
+                return true
+            }
+        }
+        return false
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    var lastTimeBackPressed: Long = 0
+    override fun onBackPressed() {
+        if (System.currentTimeMillis() - lastTimeBackPressed >= 1500) {
+            lastTimeBackPressed = System.currentTimeMillis()
+            Toast.makeText(this, "뒤로 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_LONG).show()
+        } else {
+            finish()
+        }
     }
 }
--- Android/app/src/main/java/com/example/wakeup/navigation/HomeFragment.kt
+++ Android/app/src/main/java/com/example/wakeup/navigation/HomeFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BottomFragment.newInstance] factory method to
+ * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BottomFragment : Fragment() {
+class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,7 +50,7 @@ class BottomFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BottomFragment().apply {
+            HomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
