d7c98e3da65a09556d81e8d666c57520966f323c
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-08 14:53:45 +0900

    SettingsActivity

---
+++ Android/app/src/main/java/com/example/wakeup/activities/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wakeup.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wakeup.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
