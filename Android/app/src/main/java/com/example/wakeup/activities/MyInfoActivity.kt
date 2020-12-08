62674d8ce151df40a7732c268b275d57593eb7c7
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-08 14:54:03 +0900

    My Information Activity

---
+++ Android/app/src/main/java/com/example/wakeup/activities/MyInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wakeup.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wakeup.R
+
+class MyInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_info)
+    }
+}
