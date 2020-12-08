9b707b3f17f7f3dd00380c894c023af222e5fb3d
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-08 15:13:48 +0900

    Main으로 다시 넘기기

--- Android/app/src/main/java/com/example/wakeup/activities/MyInfoActivity.kt
+++ Android/app/src/main/java/com/example/wakeup/activities/MyInfoActivity.kt
@@ -1,12 +1,20 @@
 package com.example.wakeup.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.wakeup.R
+import kotlinx.android.synthetic.main.activity_my_info.*
 
 class MyInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_info)
+
+        button2.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent) // MainActivity로 화면 전환
+            finish() // 꼭 finish()를 해줘야 함()
+        }
     }
 }
