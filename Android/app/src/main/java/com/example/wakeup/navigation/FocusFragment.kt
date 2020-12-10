d4c356928c5396c85ac95353944c8995eb20c4a2
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-10 10:03:09 +0900

    FocusFragment

--- Android/app/src/main/java/com/example/wakeup/navigation/FocusFragment.kt
+++ Android/app/src/main/java/com/example/wakeup/navigation/FocusFragment.kt
@@ -1,7 +1,6 @@
 package com.example.wakeup.navigation
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+//import com.github.clans.fab.FloatingActionButton
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,20 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.wakeup.R
-import com.github.clans.fab.FloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_focus.view.*
 
 
 class FocusFragment : Fragment() {
 
-    lateinit var mediaPlayer : MediaPlayer
+    lateinit var mediaPlayer: MediaPlayer
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view : View = inflater.inflate(R.layout.fragment_focus, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_focus, container, false)
 
 
         view.fab_Item1.setOnClickListener {
-            startMusic(view.fab_Item1, R.raw.sad)
+            startMusic(view.fab_Item1 as FloatingActionButton, R.raw.sad)
         }
         view.fab_Item1.setOnClickListener {
             var mediaPlayer = MediaPlayer.create(context, R.raw.sad)
@@ -34,16 +33,16 @@ class FocusFragment : Fragment() {
         return view
     }
 
-    fun startMusic(view: FloatingActionButton, musicId : Int){
+    fun startMusic(view: FloatingActionButton, musicId: Int) {
         //val fabIcon = BitmapFactory.decodeResource(resources, R.drawable.fab_add)
 
         //view.setImageBitmap(fabIcon)
-        view.setImageResource(R.drawable.fab_add)
+        view.setImageResource(R.drawable.baseline_headset_black_18dp)
         var mediaPlayer = MediaPlayer.create(context, musicId)
         mediaPlayer.start()
     }
 
-    fun cantExit(){
+    fun cantExit() {
 
     }
 
