1f8f302b25286e366f15edd8226a584f42ee1e57
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-10 18:09:36 +0900

    log in/sign in Button Methods

--- Android/app/src/main/java/com/example/wakeup/activities/MainActivity.kt
+++ Android/app/src/main/java/com/example/wakeup/activities/MainActivity.kt
@@ -7,11 +7,13 @@ import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wakeup.R
 import com.example.wakeup.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -59,11 +61,32 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     }
     // endregion
 
+    // region onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(this)
+
+        loginButton.setOnClickListener {
+            handleLoginDialog()
+        }
+        signInButton.setOnClickListener {
+            handleSigninDialog()
+        }
+    }
+    // endregion
+
+    private fun handleLoginDialog() {
+        var view = layoutInflater.inflate(R.layout.login_dialog, null)
+        var builder = AlertDialog.Builder(this)
+        builder.setView(view).show()
+
+
+    }
+
+    private fun handleSigninDialog() {
+        TODO("Not yet implemented")
     }
 
     // region 두 번 누르면 종료
@@ -78,7 +101,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     }
     // endregion
 
-
     // region 옵션메뉴 보이게!
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
@@ -86,7 +108,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         return true
     }
     // endregion
-
 
     // region 메뉴 아이템 선택 시
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -107,7 +128,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     }
     // endregion
 
-    //region FocusFragment에서 사용 할 특수 키 이벤트 핸들링
+    // region FocusFragment에서 사용 할 특수 키 이벤트 핸들링
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         when (keyCode) {
             KeyEvent.KEYCODE_BACK -> {
@@ -125,9 +146,12 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         }
         return super.onKeyDown(keyCode, event)
     }
+    // endregion
 
+    // region 홈 키 눌렸는지
     override fun onUserLeaveHint() {
         super.onUserLeaveHint()
         Log.d("LogTest", "Home Key Pressed")
     }
+    // endregion
 }
