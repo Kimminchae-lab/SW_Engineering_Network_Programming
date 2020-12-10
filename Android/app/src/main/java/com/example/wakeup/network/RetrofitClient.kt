5823ae8529f00150bfb8336db7c317fec567cf50
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-10 15:31:23 +0900

    RetrofitClient

--- Android/app/src/main/java/com/example/wakeup/network/RetrofitClient.kt
+++ Android/app/src/main/java/com/example/wakeup/network/RetrofitClient.kt
@@ -3,15 +3,14 @@ package com.example.wakeup.network
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-
 class RetrofitClient {
     private var retrofit: Retrofit? = null
     fun getClient(baseUrl: String?): Retrofit? {
         if (retrofit == null) {
             retrofit = Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
         }
         return retrofit
     }
