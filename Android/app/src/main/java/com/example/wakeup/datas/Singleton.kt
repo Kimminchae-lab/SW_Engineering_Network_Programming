2d7f65c7fe878c3c99a5b75c49a9495ca4c1f38c
Hotteock <57486593+Hotteock3388@users.noreply.github.com>
2020-12-14 15:58:10 +0900

    Edit Singleton

--- Android/app/src/main/java/com/example/wakeup/datas/Singleton.kt
+++ Android/app/src/main/java/com/example/wakeup/datas/Singleton.kt
@@ -1,10 +1,34 @@
 package com.example.wakeup.datas
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wakeup.model.StudyRecord
+
 
 object Singleton {
     //var reserveDataList : ArrayList<ReserveData> = ArrayList<ReserveData>()
     var mediaPlayer : MediaPlayer = MediaPlayer()
 
     var playingPosition : Int = 0
+
+    lateinit var sharedPreferences : SharedPreferences
+
+    var studyRecordList = ArrayList<StudyRecord>()
+
+    fun gMediaPlayer(): MediaPlayer {
+        return mediaPlayer
+    }
+
+    fun setMusicOfMediaPlayer(context: Context?, resId: Int){
+        mediaPlayer = MediaPlayer.create(context, resId)
+    }
+
+    fun setSharedPreference(sf: SharedPreferences){
+        sharedPreferences = sf
+    }
+    fun getSharedPreference() : SharedPreferences{
+        return sharedPreferences
+    }
 }
