4ed5b91b94529a114e5a80ab71f2259be041ed58
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-14 19:33:11 +0900

    LoginResult

--- Android/app/src/main/java/com/example/wakeup/network/LoginResult.kt
+++ Android/app/src/main/java/com/example/wakeup/network/LoginResult.kt
@@ -2,5 +2,14 @@ package com.example.wakeup.network
 
 class LoginResult {
     private val email: String = ""
+    fun getEmail(): String {
+        return email
+    }
+
     private val password: String = ""
+    fun getPW(): String {
+        return password
+    }
+
+
 }
