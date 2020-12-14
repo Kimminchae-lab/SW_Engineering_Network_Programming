aa1b0e03aac8a05fe747615bb3b2ca7e747979fc
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-14 21:00:19 +0900

    Update db.js

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -6,11 +6,12 @@ var connection = mysql.createConnection({
   database: process.env.DATABASE,
 });
 
-connection.connect();
-
-connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
-  if (error) throw error;
-  console.log("The solution is: ", results[0].solution);
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-connection.end();
+module.exports = connection;
