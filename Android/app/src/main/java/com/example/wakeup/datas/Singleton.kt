c3510fc55cd6bb41b33a9bb05a4ad92ab355843e
Hotteock <57486593+Hotteock3388@users.noreply.github.com>
2020-12-15 12:07:27 +0900

    Update Singleton.kt

--- Android/app/src/main/java/com/example/wakeup/datas/Singleton.kt
+++ Android/app/src/main/java/com/example/wakeup/datas/Singleton.kt
@@ -4,7 +4,12 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
+import com.example.wakeup.R
+import com.example.wakeup.model.ItemTodo
 import com.example.wakeup.model.StudyRecord
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlin.coroutines.coroutineContext
 
 
 object Singleton {
@@ -31,4 +36,70 @@ object Singleton {
     fun getSharedPreference() : SharedPreferences{
         return sharedPreferences
     }
+
+
+    fun loadStudyRecord(key: String) : ArrayList<StudyRecord>{
+        var itemList =  ArrayList<StudyRecord>()
+        var json = getSharedPreference().getString( key, null );
+        var gson = Gson()
+
+        val turnsType = object : TypeToken<ArrayList<StudyRecord>>(){}.type
+        if(json != null) {
+            itemList = gson.fromJson(json, turnsType)
+        }
+        return itemList
+    }
+
+    fun saveStudyRecord(dataList: ArrayList<StudyRecord>, key: String){
+        //var pref = getSharedPreferences( "pref", MODE_PRIVATE );
+        var pref_editor = getSharedPreference().edit();
+
+        //var itemList = ArrayList<StudyRecord>()
+        //itemList.add(StudyRecord(10, "a", "b"))
+        //itemList.add(StudyRecord(100, "aa", "bb"))
+
+        var gson = Gson()
+        var json = gson.toJson(dataList)
+
+        pref_editor.putString(key, json)
+        pref_editor.apply()
+    }
+
+
+
+    fun loadItemTodo( key: String) : ArrayList<ItemTodo>{
+        var itemList =  ArrayList<ItemTodo>()
+        var json = getSharedPreference().getString( key, null );
+        var gson = Gson()
+
+        val turnsType = object : TypeToken<ArrayList<ItemTodo>>(){}.type
+        if(json != null) {
+            itemList = gson.fromJson(json, turnsType)
+        }
+        return itemList
+    }
+
+    fun saveItemTodo( dataList: ArrayList<ItemTodo>, key: String){
+        //var pref = getSharedPreferences( "pref", MODE_PRIVATE );
+        var pref_editor = getSharedPreference().edit();
+
+        //var itemList = ArrayList<ItemTodo>()
+        //itemList.add(ItemTodo(10, "a", "b"))
+        //itemList.add(ItemTodo(100, "aa", "bb"))
+
+        var gson = Gson()
+        var json = gson.toJson(dataList)
+
+        pref_editor.putString(key, json)
+        pref_editor.apply()
+    }
+
+    fun clearData(key: String){
+        var pref_editor = getSharedPreference().edit();
+
+        pref_editor.clear()
+        pref_editor.apply()
+
+    }
+
 }
