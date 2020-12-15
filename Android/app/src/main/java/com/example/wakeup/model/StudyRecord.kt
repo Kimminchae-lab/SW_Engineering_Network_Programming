ca4ad72c5793523898a46196611ac2507cafc5d2
Hotteock <57486593+Hotteock3388@users.noreply.github.com>
2020-12-15 12:07:32 +0900

    Update StudyRecord.kt

--- Android/app/src/main/java/com/example/wakeup/model/StudyRecord.kt
+++ Android/app/src/main/java/com/example/wakeup/model/StudyRecord.kt
@@ -1,5 +1,9 @@
 package com.example.wakeup.model
 
+import android.content.SharedPreferences
+import com.example.wakeup.datas.Singleton
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.time.LocalDate
 
 data class StudyRecord (
@@ -8,3 +12,5 @@ data class StudyRecord (
     var details: String,
     var studyDate: String
 )
+
+
