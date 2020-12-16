86946279871b4919326ffa81199bc29beee3cf76
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-16 21:13:31 +0900

    Update ItemTodo.kt

--- Android/app/src/main/java/com/example/wakeup/model/ItemTodo.kt
+++ Android/app/src/main/java/com/example/wakeup/model/ItemTodo.kt
@@ -1,14 +1,9 @@
 package com.example.wakeup.model
 
-import android.content.SharedPreferences
-import com.example.wakeup.datas.Singleton
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-
-data class ItemTodo (
+data class ItemTodo(
     var isChecked: Boolean,
-    var text : String
-){
+    var text: String
+) {
 
 
 }
