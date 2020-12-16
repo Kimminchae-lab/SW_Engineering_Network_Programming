d78f1f2474c25f533a7ce0830553c6fce4fb96fc
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-16 23:48:28 +0900

    Update SplashActivity.kt

--- Android/app/src/main/java/com/example/wakeup/activities/SplashActivity.kt
+++ Android/app/src/main/java/com/example/wakeup/activities/SplashActivity.kt
@@ -3,31 +3,26 @@ package com.example.wakeup.activities
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wakeup.R
 import com.example.wakeup.datas.Singleton
+import com.example.wakeup.network.RetrofitClient
 import com.example.wakeup.network.RetrofitInterface
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.android.synthetic.main.login_dialog.*
-import kotlinx.android.synthetic.main.login_dialog.view.*
-import kotlinx.android.synthetic.main.signup_dialog.*
-import kotlinx.android.synthetic.main.signup_dialog.view.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import com.example.wakeup.network.RetrofitClient as
 
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var retrofit: Retrofit
     private lateinit var retrofitInterface: RetrofitInterface
     private var BASE_URL = "http://10.53.68.1:3000"
-    var logedIn: Int = 0
-    var signedIn: Int = 0
 
     lateinit var myAPI: RetrofitInterface
     var compositeDisposable = CompositeDisposable()
@@ -36,7 +31,8 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        var retrofit = RetrofitClient.
+        var retrofit: Retrofit? = RetrofitClient().Instance()
+        myAPI = retrofit!!.create(RetrofitInterface::class.java)
         initDatas()
 
 
@@ -44,10 +40,10 @@ class SplashActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
         login.setOnClickListener {
-            handleLoginDialog()
+            loginUser(email_Edit.text.toString(), pw_Edit.text.toString())
         }
         signIn.setOnClickListener {
-            handleSigninDialog()
+
         }
         retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -71,22 +67,38 @@ class SplashActivity : AppCompatActivity() {
         startLoading()
     }
 
+    private fun loginUser(email: String, pw: String) {
+        compositeDisposable.add(
+            myAPI.loginUser(email, pw)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(Consumer<String>() {
+                    
+                })
+        )
+    }
+
     private fun initDatas() {
         var sf: SharedPreferences = getSharedPreferences("sharedPreferenceFile", MODE_PRIVATE)
         Singleton.setSharedPreference(sf)
         //Singleton.clearData(applicationContext.resources.getString(R.string.user_data))
         //RecordWhatStudied().removeDataSharedPreference(Singleton.getSharedPreference())
 
-        Singleton.itemToDoList = Singleton.loadItemTodo(applicationContext.resources.getString(R.string.todo_list))
-        Singleton.studyRecordList = Singleton.loadStudyRecord( applicationContext.resources.getString(R.string.study_record_list))
-        Singleton.userProfile = Singleton.loadUserData(applicationContext, applicationContext.resources.getString(R.string.user_data))
+        Singleton.itemToDoList =
+            Singleton.loadItemTodo(applicationContext.resources.getString(R.string.todo_list))
+        Singleton.studyRecordList =
+            Singleton.loadStudyRecord(applicationContext.resources.getString(R.string.study_record_list))
+        Singleton.userProfile = Singleton.loadUserData(
+            applicationContext,
+            applicationContext.resources.getString(R.string.user_data)
+        )
 
         //Singleton.clearData(applicationContext.resources.getString(R.string.todo_list))
     }
 
     // 앱 실행 후 2초 뒤에 MainActivity로 전환
     private fun startLoading() {
-        /*Handler().postDelayed({
+        Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent) // MainActivity로 화면 전환
             finish() // 꼭 finish()를 해줘야 함
