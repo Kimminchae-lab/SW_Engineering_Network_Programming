c17c9a06b286052ea943dfa98a8c124fee809f29
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-16 21:13:16 +0900

    Update index.js

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var mysql = require("mysql");
+var parser = require("body-parser");
 var app = express();
-var path = require("path");
 var router = express.Router();
 
-// Create Connection with MySQL
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -12,33 +11,65 @@ var connection = mysql.createConnection({
   database: "wakeup",
 });
 
-connection.connect();
+//connection.connect();
 
-router.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../../public/join.html"));
-});
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
 
-router.post("/", function (req, res) {
-
-  var email = req.body.email;
+app.post("/register", (req, res, next) => {
   var name = req.body.name;
+  var email = req.body.email;
   var pw = req.body.password;
 
-  var query = connection.query(
-    'insert into user (username, userpw, useremail) values ("' +
-      email +
-      '","' +
-      name +
-      '","' +
-      pw +
-      '")',
-    function (err, rows) {
-      if (err) {
-        throw err;
+  connection.query(
+    "SELECT * FROM user WHERE email = ?",
+    [email],
+    function (err, result, fields) {
+      connection.on("error", function (err) {
+        console.log("[MySQL ERROR]", err);
+      });
+      var sql =
+        "INSERT INTO user (username, userpw, useremail), VALUES (?, ?, ?)";
+      var params = [name, pw, email];
+      if (result && result.length) {
+        res.json("User already exists!!!");
+      } else {
+        connection.query(sql, params, function (err, rows, fields) {
+          connection.on("error", function (err) {
+            console.log("[MySQL ERROR]", err);
+            res.json("Register error: ", err);
+          });
+          res.json("Register successful");
+        });
       }
-      console.log("Data inserted!");
     }
   );
 });
 
+app.post("/login/", (req, res, next) => {
+  connection.query(
+    "SELECT * FROM user WHERE email = ?",
+    [email],
+    function (err, result, fields) {
+      connection.on("error", function (err) {
+        console.log("[MySQL ERROR]", err);
+      });
+      var sql =
+        "INSERT INTO user (username, userpw, useremail), VALUES (?, ?, ?)";
+      var params = [name, pw, email];
+      if (result && result.length) {
+        if (result[0] == pw) {
+          res.end(JSON.stringify(result[0]));
+        } else {
+          res.end(JSON.stringify("Wrong password"));
+        }
+      } else {
+        res.json("Loged In");
+      }
+    }
+  );
+});
+app.listen(3000, () => {
+  console.log("Running");
+});
 module.exports = router;
