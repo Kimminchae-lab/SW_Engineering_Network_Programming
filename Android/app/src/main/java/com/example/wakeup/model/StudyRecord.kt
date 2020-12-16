a0f28475381b531b3ccf5f7c682e0550cadbbbc0
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-16 21:13:26 +0900

    Update StudyRecord.kt

--- Android/app/src/main/java/com/example/wakeup/model/StudyRecord.kt
+++ Android/app/src/main/java/com/example/wakeup/model/StudyRecord.kt
@@ -1,14 +1,8 @@
 package com.example.wakeup.model
 
-import android.content.SharedPreferences
-import com.example.wakeup.datas.Singleton
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import java.time.LocalDate
-
-data class StudyRecord (
-    var studyTime : Int,
-    var summary : String,
+data class StudyRecord(
+    var studyTime: Int,
+    var summary: String,
     var details: String,
     var studyDate: String
 )
