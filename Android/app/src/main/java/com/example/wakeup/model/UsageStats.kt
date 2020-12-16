b9fa838a9ae2c2820ee5cf0171abcfcb039054bd
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-16 21:13:23 +0900

    Update UsageStats.kt

--- Android/app/src/main/java/com/example/wakeup/model/UsageStats.kt
+++ Android/app/src/main/java/com/example/wakeup/model/UsageStats.kt
@@ -3,9 +3,9 @@ package com.example.wakeup.model
 import android.graphics.drawable.Drawable
 
 data class UsageStats(
-        var appName : kotlin.String,
-        var appIcon : Drawable,
-        var useTime: Int
+    var appName: kotlin.String,
+    var appIcon: Drawable,
+    var useTime: Int
 ) {
     fun getTime(): Int {
         return useTime
