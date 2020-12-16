c1ac80418778ea407b4a7a8923f87108db3caf66
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-16 23:48:33 +0900

    Update RetrofitClient.kt

--- Android/app/src/main/java/com/example/wakeup/network/RetrofitClient.kt
+++ Android/app/src/main/java/com/example/wakeup/network/RetrofitClient.kt
@@ -4,11 +4,10 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
-class RetrofitClient {
-    private var instance: Retrofit? = null
-    private var retrofit: Retrofit? = null
+class RetrofitClient() {
+    var instance: Retrofit? = null
 
-    fun getInstance(): Retrofit? {
+    fun Instance(): Retrofit? {
         if (instance == null) {
             instance = Retrofit.Builder()
                 .baseUrl("http://10.53.68.141.3000/")
